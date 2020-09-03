bc87ce3b5699e7ed429253eb9f7a09fd64ed0df2
raviparmar-coditas <ravi.parmar@coditas.com>
2020-09-03 17:47:06 +0530

    setup for new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpService } from './services/http.service';
+import { StateService } from './services/state.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test-assignment';
+  products: any;
+  constructor(
+    private httpService: HttpService,
+    public stateService: StateService
+  ) {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpService.getSecured(environment.login).subscribe((data) => {
+      this.products = data;
+      console.log(this.products);
+    });
+  }
+
+  getId(id) {
+    console.log(id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { LoginComponent } from './components/login/login.component';
+import { ButtonComponent } from './common-components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    LoginComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
