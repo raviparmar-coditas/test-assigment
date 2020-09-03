f4658d2995b66ff16348e4790facb2abd1603854
raviparmar-coditas <ravi.parmar@coditas.com>
2020-09-03 17:42:37 +0530

    card component created

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  @Input() heading;
+  @Input() description;
+  @Input() imageUrl;
+  constructor() {
+    
+   }
+
+  ngOnInit(): void {
+  }
+
+
+
+  
+
+}
